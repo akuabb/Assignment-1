671634e8ed0c66c9e922b5786c0b3925e39bd15b
David Dzineku <bigthinzcount@gmail.com>
2021-11-11 17:46:11 +0000

    initial commit

---
+++ boat.py
@@ -0,0 +1,17 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> F = [45.5, 78.5, 45.3]
+>>> I = [54, 52, 96, 87, 45, 68, 78, 98, 21, 65, 14, 52]
+>>> S = ['Ama', 'Kojo', 'Yhup', 'Kwame', 'Kwesi', 'Roland', 'Nti', 'William', 'Patty', 'Adwoa', 'Pappy', 'Folly', 'Pency']
+>>> B = [True, False, True, False ]
+>>> 
+>>> 
+>>> 
+>>> 
+>>> S.sort()
+>>> S
+['Adwoa', 'Ama', 'Folly', 'Kojo', 'Kwame', 'Kwesi', 'Nti', 'Pappy', 'Patty', 'Pency', 'Roland', 'William', 'Yhup']
+>>> B.sort()
+>>> B
+[False, False, True, True]
+>>> 
